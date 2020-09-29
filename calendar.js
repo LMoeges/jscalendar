d3c264e8ecba7df489ba3c186e450a32c0cb8a45
LMoeges <48687232+LMoeges@users.noreply.github.com>
2020-09-30 00:19:07 +0200

    Calendar now generates missing months during clickthrough

--- calendar.js
+++ calendar.js
@@ -8,6 +8,10 @@ var calConfig = {
 	stylesheet:"./calendar.css"
 };
 
+const monthIdPrefix = "MID_";
+const yearIdPrefix = "YID_";
+const dayIdPrefix = "DID_";
+
 function generateCalendar(config){
 	if(!config)
 		throw new Error("Configuration must not be empty! For more information see https://github.com/LMoeges/jscalendar");
@@ -31,42 +35,45 @@ function generateCalendar(config){
 
 
 	//Get the root element
-	var rootElement = document.querySelector(calConfig.rootElement);
+	var rootElement = get(calConfig.rootElement);
 	
-	let year = new Date();
+	let now = new Date();
 	let calendar= document.createElement("div");
 	calendar.classList.add("calendar");
-	for(i = 0; i < 12; i++){
-		calendar.appendChild(generateMonth(year.getFullYear(), i));		
-	}
+	for(i = 0; i < 12; i++)
+		calendar.appendChild(generateMonth(now.getFullYear(), i));		
+	
 
 	rootElement.appendChild(calendar);
 	
 	// Set the current month visible
-	calendar.children[year.getMonth()].style.display="block";
+	calendar.children[0].style.display="block";
 }
 
 
 function generateMonth(year, month){
 	let monthDiv = createClassDiv("month");
-	monthDiv.id= month;
+	let monthId =month<10?`${monthIdPrefix}${year}-0${month}`:`${monthIdPrefix}${year}-${month}` 
+	monthDiv.id= monthId;
 	
 	//Set the month Text
 	let monthSpan = document.createElement("span");
 	monthSpan.classList.add("monthname");
-	monthSpan.innerText= calConfig.months[month]+ " "+ year;
+	monthSpan.innerText= `${calConfig.months[month]} ${year}`;
 	
 	//Create prev and next buttons
 	let prev = document.createElement("button");
 	prev.classList.add("prev");
-	prev.id=month;
-	prev.addEventListener("click", ()=>changeMonth(month, 11));
+	prev.id=monthId;
+	//prev.addEventListener("click", ()=>changeMonth(month, 11));
+	prev.addEventListener("click", ()=>changeMonth(monthId, -1));
 	prev.innerHTML ="&#x276E;";
 	
 	let next = document.createElement("button");
 	next.classList.add("next");
-	next.id=month;
-	next.addEventListener("click", ()=>changeMonth(month, 1));
+	next.id=monthId;
+	//next.addEventListener("click", ()=>changeMonth(month, 1));
+	next.addEventListener("click", ()=>changeMonth(monthId, 1));
 	next.innerHTML ="&#x276F;";
 	
 	//Create Monthly header
@@ -88,25 +95,25 @@ function generateMonth(year, month){
 		days.appendChild(generateBox());
 	//Add Calendar Days
 	for(j = 1; j<=neededBoxes; j++)
-		days.appendChild(generateBox(j, month));
+		days.appendChild(generateBox(j, month, year));
 	
 	monthDiv.appendChild(days);
 	return monthDiv;
 }
 
 //Function for generating the Calendardays
-function generateBox(date, month){
+function generateBox(date, month, year){
 	var box = document.createElement("div");
 	if(!date)
 		box.classList.add("day-invisible");
 	else{		
 		box.classList.add("day");
-		box.id=""+new Date().getFullYear()+"-"+(month+1)+"-"+date;
+		box.id=`${dayIdPrefix}${year}-${month + 1}-${date}`;
 
 		//If the weekdays should be displayed, add them here...
 		if(calConfig.showWeekdays){
 			let weekday = createClassSpan("weekday");
-			weekday.innerText= calConfig.days[getWeekDayIncOffset(new Date().getFullYear(), month, date)];
+			weekday.innerText= calConfig.days[getWeekDayIncOffset(year, month, date)];
 			box.appendChild(weekday);
 		}
 
@@ -145,10 +152,48 @@ function getWeekDayIncOffset(year, month, day){
 	return (new Date(year,month,day).getDay()+calConfig.firstDayOfWeekOffset)%7;
 }
 
-function changeMonth(month, value){
-	let months = document.getElementsByClassName("month");
-	months[month%12].style.display= "none";
-	months[(month+value)%12].style.display= "block";
+function changeMonth(currMonthId, value){
+	
+	//Get the selected year and month from the clicked button id.
+	let year = Number(currMonthId.match(/\d{4}/g)[0]);
+	let month = Number(currMonthId.match(/-\d\d/g)[0].substring(1));
+	let currMonthDiv = get(`#${currMonthId}`);
+	let nextMonthDiv;
+
+	let nextMonthNumber= month+(value);
+
+	//Try to get the next month div for
+	if(nextMonthNumber<0)
+		nextMonthDiv = get(`#${monthIdPrefix}${year-1}-11`);
+	else if(nextMonthNumber>11)
+		nextMonthDiv=get(`#${monthIdPrefix}${year+1}-00`);
+	else if(nextMonthNumber<10)
+		nextMonthDiv=get(`#${monthIdPrefix}${year}-0${month+(value)}`);
+	else
+		nextMonthDiv=get(`#${monthIdPrefix}${year}-${month+(value)}`);
+
+	//If the month currently does not exist, generate it. In the future this probatly will be configurable via flag.
+	if(nextMonthDiv == null){
+		let monthTmp;
+		if(nextMonthNumber<0){
+			monthTmp = generateMonth(year-1, 11);
+			get(".calendar").insertBefore(monthTmp, currMonthDiv);
+
+		}
+		else if(nextMonthNumber>11){
+			monthTmp = generateMonth(year+1,0);
+			get(".calendar").appendChild(monthTmp); 
+
+		}
+		else{
+			monthTmp = generateMonth(year, nextMonthNumber);
+			get(".calendar").appendChild(monthTmp);
+		}
+		nextMonthDiv = monthTmp;		
+	}
+
+	currMonthDiv.style.display= "none";
+	nextMonthDiv.style.display="block";
 }
 
 function loadStyles(){
@@ -157,4 +202,8 @@ function loadStyles(){
 	stylesheet.type="text/css";
 	stylesheet.href=calConfig.stylesheet;
 	document.querySelector("head").appendChild(stylesheet);
+}
+
+function get(element){
+	return document.querySelector(element);
 }
